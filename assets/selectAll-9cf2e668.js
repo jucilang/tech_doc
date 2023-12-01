16fa11fb8e09b6ed223b63e48c33acdf3cfa0d5c
xiaoliang <420115045@qq.com>
2023-12-01 17:24:16 +0800

    add resource

---
+++ assets/selectAll-9cf2e668.js
@@ -0,0 +1,2 @@
+import{a1 as t,a2 as n,a3 as o}from"./index-78b054b1.js";function r(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{r as s};
+//# sourceMappingURL=selectAll-9cf2e668.js.map
